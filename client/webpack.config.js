9aea90fb66cee0341cae766c4e5b35e064a429ed
Bogdan Tkachuk <cnndo14@gmail.com>
2024-03-06 13:00:19 -0500

    added some edits to database.js and comments

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+// Workbox plugins for a service worker and manifest file.
 module.exports = () => {
   return {
     mode: 'development',
@@ -14,7 +12,7 @@ module.exports = () => {
       install: './src/js/install.js',
       database: './src/js/database.js',
       editor: './src/js/editor.js',
-      header: './src/js/header.js'
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -43,12 +41,12 @@ module.exports = () => {
           {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('asserts', 'icons'),
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
     ],
-
+// CSS loaders and Babel.
     module: {
       rules: [
         {
